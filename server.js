bfff0dda486c525cd7c1beb392a1b5693b919bc2
Doug Colaizzo <dcolaizzo@marathondata.com>
2013-12-07 12:27:43 -0500

    Hey YUNO show windows. Check platform.

--- server.js
+++ server.js
@@ -1,87 +1,95 @@
 var http = require('http'),
-	express = require('express'),
-	ws = require('ws'),
-	x11 = require('x11');
+    express = require('express'),
+    ws = require('ws'),
+    os = require('os'),
+    platform = os.platform();
 
-var keyMap = [];
-keyMap[17] = 37 //control;
+if (platform !== "win32") {
+    var x11 = require('x11');
+    var keyMap = [];
+    keyMap[17] = 37 //control;
 
 
-x11.createClient(function(error,display) {
+    x11.createClient(function(error, display) {
 
-	if(error) throw error;
+        if (error) throw error;
 
-	var SENSITIVITY = 2;
+        var SENSITIVITY = 2;
 
-	var client = display.client,
-		root = display.screen[0].root;
+        var client = display.client,
+            root = display.screen[0].root;
 
-	var handleMessage = function(message) {
-		var parsed = JSON.parse(message);
-		if(parsed.x || parsed.y) {
-			client.QueryPointer(root, function(error,pos) {
-				var x = pos.rootX + parsed.x * SENSITIVITY;
-				var y = pos.rootY + parsed.y * SENSITIVITY;
-				client.WarpPointer(0,root,0,0,0,0,x,y);
-			});
-		}
-		if(parsed.hasOwnProperty('mouseDown')) {
-			client.require('xtest', function(Test) {
-				Test.FakeInput(Test.ButtonPress, parsed.mouseDown, 0, root, 0, 0);
-			});
-		}
+        var handleMessage = function(message) {
+            var parsed = JSON.parse(message);
+            if (parsed.x || parsed.y) {
+                client.QueryPointer(root, function(error, pos) {
+                    var x = pos.rootX + parsed.x * SENSITIVITY;
+                    var y = pos.rootY + parsed.y * SENSITIVITY;
+                    client.WarpPointer(0, root, 0, 0, 0, 0, x, y);
+                });
+            }
+            if (parsed.hasOwnProperty('mouseDown')) {
+                client.require('xtest', function(Test) {
+                    Test.FakeInput(Test.ButtonPress, parsed.mouseDown, 0, root, 0, 0);
+                });
+            }
 
-		if(parsed.hasOwnProperty('mouseUp')) {
-			client.require('xtest', function(Test) {
-				Test.FakeInput(Test.ButtonRelease, parsed.mouseUp, 0, root, 0, 0);
-			});
-		}
+            if (parsed.hasOwnProperty('mouseUp')) {
+                client.require('xtest', function(Test) {
+                    Test.FakeInput(Test.ButtonRelease, parsed.mouseUp, 0, root, 0, 0);
+                });
+            }
 
-		if(parsed.hasOwnProperty('keyDown')) {
-			client.require('xtest', function(Test) {
-				var key = keyMap[parsed.keyDown];
-				if(key)
-					Test.FakeInput(Test.KeyPress, key, 0, root, 0, 0);
-			});
-		}
+            if (parsed.hasOwnProperty('keyDown')) {
+                client.require('xtest', function(Test) {
+                    var key = keyMap[parsed.keyDown];
+                    if (key)
+                        Test.FakeInput(Test.KeyPress, key, 0, root, 0, 0);
+                });
+            }
 
-		if(parsed.hasOwnProperty('keyUp')) {
-			client.require('xtest', function(Test) {
-				var key = keyMap[parsed.keyUp];
-				if(key)
-					Test.FakeInput(Test.KeyRelease, key, 0, root, 0, 0);
-				else
-					console.log("Couldn't find "+key);
-			});
-		}
+            if (parsed.hasOwnProperty('keyUp')) {
+                client.require('xtest', function(Test) {
+                    var key = keyMap[parsed.keyUp];
+                    if (key)
+                        Test.FakeInput(Test.KeyRelease, key, 0, root, 0, 0);
+                    else
+                        console.log("Couldn't find " + key);
+                });
+            }
 
-		if(parsed.hasOwnProperty('info'))
-			console.log(parsed.info);
-	};
+            if (parsed.hasOwnProperty('info'))
+                console.log(parsed.info);
+        };
 
-	var app = express();
+        var app = express();
 
-	var httpServer = http.createServer(app);
+        var httpServer = http.createServer(app);
 
-	app.use(express["static"](__dirname + '/client'));
+        app.use(express["static"](__dirname + '/client'));
 
-	httpServer.listen(9000);
+        httpServer.listen(9000);
 
-	var wsServer = new ws.Server({server: httpServer});
+        var wsServer = new ws.Server({
+            server: httpServer
+        });
 
-	wsServer.on('connection', function(ws) {
-		console.log("Connected");
+        wsServer.on('connection', function(ws) {
+            console.log("Connected");
 
-		ws.on('message', handleMessage);
+            ws.on('message', handleMessage);
 
-		ws.on('error', function(error) {
-			console.error(error);
-		});
+            ws.on('error', function(error) {
+                console.error(error);
+            });
 
-		ws.on('close', function(message) {
-			console.log("Close "+message);
-		});
-	});
+            ws.on('close', function(message) {
+                console.log("Close " + message);
+            });
+        });
 
-});
+    });
 
+} else {
+    console.log("Windows version here");
+}
